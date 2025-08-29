6ed5dc5aa68cbcfaa4be4957f06bf3e4e51da937
Marc Julian Boettcher <boettchm@rhrk.uni-kl.de>
2025-08-29 17:53:31 +0200

    Small fix in host setup

--- src/simulation.py
+++ src/simulation.py
@@ -24,7 +24,7 @@ def run_simulation(df, experiment_name):
 
     network = Network(env, logger=logger)
 
-    
+
     host_dict = {
         "host_0": host1,
         "host_1": host2
@@ -34,10 +34,8 @@ def run_simulation(df, experiment_name):
         # Assign each task to exactly one host.
         task_id = row["id"]
 
-        #host    = host1 if row["host_preference"] == "host_0" else host2
-        #host_map[task_id] = host
-
-        host = host_dict.get(row["host_preference"]) 
+        host = host_dict.get(row["host_preference"])
+        host_map[task_id] = host
 
 
         # If task has dependency, enter it as successor
